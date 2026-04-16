2e872c5f0dc1861d06090a122684e533fe2435c7
Cynthia-Liao <c920705a@gmail.com>
2026-04-17 00:18:05 +0800

    Adjust controller spec

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -16,39 +16,41 @@ module LockedCV
 
     route do |routing|
       response['Content-Type'] = 'application/json'
+
       routing.root do
         { message: 'LockedCV API up at /api/v1' }.to_json
       end
 
-      routing.on 'api' do
-        routing.on 'v1' do
-          routing.on 'personal_data' do
-            # GET api/v1/personal_data/[id]
-            routing.get String do |id|
-              PersonalData.find(id).to_json
-            rescue StandardError
-              routing.halt 404, { message: 'Personal data not found' }.to_json
-            end
+      @api_root = 'api/v1'
+      routing.on @api_root do
+        routing.on 'personal_data' do
+          @personal_data_route = "#{@api_root}/personal_data"
 
-            # GET api/v1/personal_data
-            routing.get do
-              output = { personal_data_ids: PersonalData.all }
-              JSON.pretty_generate(output)
-            end
+          # GET api/v1/personal_data/[id]
+          routing.get String do |id|
+            personal_data = PersonalData.find(id)
+            personal_data ? personal_data.to_json : raise('Personal data not found')
+          rescue StandardError
+            routing.halt 404, { message: 'Personal data not found' }.to_json
+          end
 
-            # POST api/v1/personal_data
-            routing.post do
-              new_data = JSON.parse(routing.body.read)
-              new_doc = PersonalData.new(new_data)
-              if new_doc.save_changes
-                response.status = 201
-                { message: 'Personal data saved', id: new_doc.id }.to_json
-              else
-                routing.halt 400, { message: 'Could not save personal data' }.to_json
-              end
-            rescue JSON::ParserError
-              routing.halt 400, { message: 'Could not save personal data' }.to_json
-            end
+          # GET api/v1/personal_data
+          routing.get do
+            output = { personal_data_ids: PersonalData.all }
+            JSON.pretty_generate(output)
+          end
+
+          # POST api/v1/personal_data
+          routing.post do
+            new_data = JSON.parse(routing.body.read)
+            new_doc = PersonalData.new(new_data)
+            raise('Could not save personal data') unless new_doc.save_changes
+
+            response.status = 201
+            response['Location'] = "#{@personal_data_route}/#{new_doc.id}"
+            { message: 'Personal data saved', id: new_doc.id }.to_json
+          rescue StandardError
+            routing.halt 400, { message: 'Could not save personal data' }.to_json
           end
         end
       end
