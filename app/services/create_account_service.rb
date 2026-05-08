32a0d4425625122a1a737d008bc53c1640f659ba
linda123456789123456789 <linda109101503@g.ncu.edu.tw>
2026-05-09 01:10:32 +0800

    fixed: 建立新帳號後，會自動加上預設 member role

--- app/services/create_account_service.rb
+++ app/services/create_account_service.rb
@@ -4,7 +4,14 @@ module LockedCV
   # Creates an account from API payload.
   class CreateAccountService
     def self.call(account_data:)
-      Account.create(account_data)
+      account = Account.create(account_data)
+      account.add_system_role(default_member_role)
+      account
     end
+
+    def self.default_member_role
+      Role.find_or_create(name: 'member')
+    end
+    private_class_method :default_member_role
   end
 end
